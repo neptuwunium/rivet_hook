5c4adf0b240f5d92f46b091d991c8f7633419c26
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-07 16:03:48 +0530

    hook: separate to branch

--- Rivet.IO/IStringPooled.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025-2026 Neptuwunium
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-namespace Rivet.IO;
-
-public interface IStringPooled {
-	string GetString(int offset);
-}
--- Rivet/Models/RivetAssetReference.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025-2026 Neptuwunium
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-using Rivet.IO;
-
-namespace Rivet.Models;
-
-public record RivetAssetReference(RivetAssetId AssetId, string Path, RivetTypeId TypeId);
